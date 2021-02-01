073803debb96e1ce96dbdc1f47943bce3f567ff5
islamardak <60878180+islamardak@users.noreply.github.com>
2021-02-01 17:11:21 +0600

    project

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function (){
+$('.slider').slick();
+        /*
+        prevArrow: $('.prev'),
+        nextArrow: $('.next'),
+        dots: true,
+        speed: 400,
+        draggable: false,
+        responsive: [
+          {
+            breakpoint: 576,
+            settings: {
+              adaptiveHeight: true,
+              adaptiveWidth: true
+            }
+          }]
+      });
+*/
+});
